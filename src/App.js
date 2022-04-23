d953f8d768fff0dd2651a1c28d1fa4c361af74a0
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-23 23:44:44 +0530

    install required dependency

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import React from "react";
 const App = () => {
     return (
         <div>
-            <h1>
-                Travel Adviser
-            </h1>
+            <h1>Travel Adviser</h1>
         </div>
     );
 }
