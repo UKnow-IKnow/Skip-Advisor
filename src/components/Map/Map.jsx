5bfa460c86fa9432dca9fa6f2f627fdf7a6f84fa
UKnow-IKnow <sarojsahu014@gmail.com>
2022-05-04 20:56:53 +0530

    Design list part

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,7 +4,7 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import  Rating  from '@material-ui/lab';
 
-import useStyles from './styles.js';
+import useStyles from './styles';
 
 const Map = () => {
     const classes = useStyles();
