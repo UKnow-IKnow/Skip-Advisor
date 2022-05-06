39868b80a1a9bc92fb70b805a4ef04845897d6b7
UKnow-IKnow <sarojsahu014@gmail.com>
2022-05-06 23:41:00 +0530

    bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,28 @@
-import axios from 'axios'; //this library used for api call
+import axios from "axios"; //this library used for api call
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-    'X-RapidAPI-Key': '18cf2c46bfmshedf2e1e3ffd8c31p12e0f6jsn5d0b541feae8'
+export const getPlacesData = async (sw, ne) => {
+  try {
+    //request
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        "X-RapidAPI-Key": "18cf2c46bfmshedf2e1e3ffd8c31p12e0f6jsn5d0b541feae8",
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
   }
 };
-
-
-export const getPlacesData = async () => {
-    try {
-        //request
-        const { data: {data} } = await axios.get(URL, options);
-
-        return data;
-    }catch (error) {
-        console.log(error)
-    }
-}
