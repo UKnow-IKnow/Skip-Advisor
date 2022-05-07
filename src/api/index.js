12b7a9d84341c04541a6debfee302b22c83324e7
UKnow-IKnow <sarojsahu014@gmail.com>
2022-05-07 14:42:48 +0530

    Design card media of placeDetails

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,15 @@ const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary"
 
 export const getPlacesData = async (sw, ne) => {
   try {
-    const { data: { data } } = await axios.get( URL, {
+    //request
+    const {
+      data: { data },
+    } = await axios.get(URL, {
       params: {
         bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
         bl_longitude: sw.lng,
         tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
       },
       headers: {
         "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,22 +5,12 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails'
 
 import useStyles from './styles';
 
-const List = () => {
+const List = ({ places }) => {
     const classes = useStyles();
-    const [type, setType] = useState('attractions');
+    const [type, setType] = useState('restaurants');
     const [rating, setRating] = useState('0');
 
-    const places = [
-        { name: 'hot place' },
-        { name: 'chill Beer' },
-        { name: 'Best Steak' },
-        { name: 'hot place' },
-        { name: 'chill Beer' },
-        { name: 'Best Steak' },
-        { name: 'hot place' },
-        { name: 'chill Beer' },
-        { name: 'Best Steak' }
-    ];
+
 
 
     return(
