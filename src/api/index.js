ec442c5d79dd3f502f26a4cde69f691bfb47f1a2
UKnow-IKnow <sarojsahu014@gmail.com>
2022-05-30 23:22:35 +0530

    random

--- src/api/index.js
+++ src/api/index.js
@@ -38,10 +38,10 @@ export const getWeatherData = async(lat, lng) => {
       headers: {
         'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
         'X-RapidAPI-Key': '18cf2c46bfmshedf2e1e3ffd8c31p12e0f6jsn5d0b541feae8'
-      }
+      },
     });
     return data;
   }catch(error){
     console.log(error)
   }
-}
+};
